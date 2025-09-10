571d2eb1d3b789fb4697668b3379926bb538c42f
Lechu000 <eze0317@gmail.com>
2025-09-10 17:40:41 -0300

    dasdsa

---
+++ Assets/ColisionEjecute.cs
@@ -0,0 +1,38 @@
+using UnityEngine;
+[RequireComponent(typeof(Rigidbody))]
+public class ColisionEjecute : MonoBehaviour
+{
+    [SerializeField] private Animator _animator;
+    private bool _boolValue=false;
+    [SerializeField]private Rigidbody _rb;
+    [SerializeField] private float _vel=3.2f;
+    private float _timer = 0;
+    private void Start()
+    {
+        _animator.speed = 0;
+        if(_animator==null)
+        {
+            _animator = GetComponent<Animator>();
+        }
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        PjModel model = other.GetComponent<PjModel>();
+        if (other != null && _animator != null)
+        {
+            _boolValue = true;
+            _animator.speed = 1;
+        }
+    }
+    private void FixedUpdate()
+    {
+        if(_boolValue)
+        {
+            _timer += Time.deltaTime;
+            if (_timer >= 2)
+            {
+                _rb.MovePosition(transform.position + Vector3.up * _vel * Time.fixedDeltaTime);
+            }
+        }
+    }
+}
