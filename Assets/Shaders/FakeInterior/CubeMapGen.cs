ffd5b065729f119fb196c53eb2882c22e03ed08c
LeZaikoo <eros.fernandez@davinci.edu.ar>
2025-11-12 07:35:20 -0300

    AssetsFakeInteriors

---
+++ Assets/Shaders/FakeInterior/CubeMapGen.cs
@@ -0,0 +1,37 @@
+using UnityEditor;
+using UnityEngine;
+
+public class CubeMapGen : ScriptableWizard
+{
+    [SerializeField] Camera _camera;
+
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void OnWizardUpdate()
+    {
+        helpString = "Selecciona la camara para renderear un Cubemap";
+        isValid = (_camera != null);
+    }
+
+    private void OnWizardCreate()
+    {
+        Cubemap cubemap = new Cubemap(512, TextureFormat.ARGB32, false);
+        _camera.RenderToCubemap(cubemap);
+        AssetDatabase.CreateAsset(cubemap, $"Assets/Shaders/FakeInterior/{_camera.name}.cubemap");
+    }
+
+    [MenuItem("Toolbox/Cubemap Wizard")]
+    static void RenderCubemap()
+    {
+        DisplayWizard<CubeMapGen>("Render Cubemap", "Render");
+    }
+}
